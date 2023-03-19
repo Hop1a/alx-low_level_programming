74ea1e1de350ddb50ea65ccc4322634d5a8efe16
Hope1a <abdiabebe4@gmaiil.com>
2023-03-19 16:29:13 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 /**
- * main - a program that prints the size of video of virious computer types
- * Return 0 (Success)
+ * main -Entry point
+ *
+ * Return:Always 0 (Success)
  *
  */
 int main(void)
-{       
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float f;
-
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
+{
+printf("size of char: %d byte(s)\n", sizeof(char));
+printf("size of int: %d byte(s)\n", sizeof(int));
+printf("size of long int: %d byte(s)\n", sizeof(long int));
+printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
